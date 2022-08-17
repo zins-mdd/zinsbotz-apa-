248497e313cf516886c0eac719fbe18cedb9d5b0
zins-mdd <111218997+zins-mdd@users.noreply.github.com>
2022-08-17 13:22:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6281511480762'],
-  ['6281511480762', 'YahyaGanzz', true]
+  ['6282118415702'],
+  ['6282118415702', 'ZINS STORE REAL', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -41,8 +41,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'AzzBot-Md'
-  var sticker_author = 'YahyaGanzz'
+  var sticker_name = 'ZinsBot-Md'
+  var sticker_author = 'ZINS STORE REAL'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -59,7 +59,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = 'AzzBot-Yahya'
+global.wm = 'ZinsBot-Md'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
